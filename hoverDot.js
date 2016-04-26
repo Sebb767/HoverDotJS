06ca9fac4a3302ac50d52c097f8a7d6201b92ed5
Sebastian Kaim <sebb@sebb767.de>
2016-04-26 13:51:19 +0200

    more Basic Plugin Layout

--- hoverDot.js
+++ hoverDot.js
@@ -6,7 +6,16 @@
 
     // register the plugin
     $.fn.dotHover = function (options) {
-        
+
+        var settings = $.extend({
+            // defaults
+            
+
+        }, options );
+
+        return this.each(function() {
+            // init each element
+        });
     };
 
 })(jQuery);
